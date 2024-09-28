d32bf4dd74f8cf96213f3e516638a6bf46b51b19
Pari Gulati <pgulati182@gmail.com>
2024-09-28 13:13:53 -0400

    Initial Commit

---
+++ RecallChecker/RecallCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallCheckerApp.swift
+//  RecallChecker
+//
+//  Created by Pari Gulati on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RecallCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
