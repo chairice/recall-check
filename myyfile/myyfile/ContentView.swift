22e88f2927ac4afe3790eed2e1283790c12c447a
Casey Feng <caseyyfeng@gmail.com>
2024-09-28 22:40:38 -0400

    rgerg

---
+++ myyfile/myyfile/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  myyfile
+//
+//  Created by Casey Feng on 9/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()rgbuhwaue
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myyfile/myyfileTests/myyfileTests.swift
@@ -0,0 +1,16 @@
+//
+//  myyfileTests.swift
+//  myyfileTests
+//
+//  Created by Casey Feng on 9/28/24.
+//
+
+import Testing
+
+struct myyfileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
